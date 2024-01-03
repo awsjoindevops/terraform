87d22bd20498a8008d5897093826e217192be754
awsdevopsonline <punnam.elite@gmail.com>
2024-01-03 21:44:37 +0530

    Terraform commit

---
+++ session-01/ec2/variables.tf
@@ -0,0 +1,26 @@
+variable "sg-name" {
+    type = string
+    default = "roboshop-all-aws"
+  
+}
+
+variable "sg-desc" {
+    type = string
+    default = "Allow TLS inbound traffic"
+  
+}
+
+
+variable "inbound-from-port" {
+    type = number
+    default = 0 
+  
+}
+
+variable "cidr_blocks" { 
+    type = list
+    default = ["0.0.0.0/0"]  
+}
+
+
+
